e2408f24608cfd5f6adfd436a8f040930f34fd8e
user <arius005@inbox.lv>
2019-10-03 16:08:40 +0300

    20191002_16_09

---
+++ darbi/char.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+{
+		//int vietā bija char darbojās
+
+char a = 10; //0000 0000 0000 0000 0000 0000 0000 1010
+printf("operācija <<: %d<<1 rezultāts %d\n", a, a<<1);//ko??? jābūt 20??
+
+char b; //parādīsies mainīgais b 4 byte
+printf("B mainīgā sākuma stāvoklis(uzreiz pec deklaresanas): %d\n", b);
+
+b=a<<2; //2 operācijas piešķiršana un bīdīšana bpa bitiem pa kreisi
+printf("operācijas <<: %d<<2 rezultāts %d\n", a, a<<2);
+
+char c; //pagaidām nekas
+printf("Papētīsim << operāciju\n----------------------------\n");
+printf("Cienījamais lietotāj, lūdzu ievadi operācijas 1. operandu: ");
+scanf("%d", &a);
+printf("Cienījamais lietotāj, lūdzu ievadi operācijas 2. operantu: ");
+scanf("%d", &c);
+b=a<<c;
+printf("------------------------------------\nSkaties, kas ir sanācis: %d\n",b);
+
+}
+
