70f4624deec0acd4120ff02f4cb760ce8b4e329d
Aris <arius005@inbox.lv>
2019-11-08 19:07:53 +0200

    20191108_19_07

--- darbi/float_in_out_file.c
+++ darbi/float_in_out_file.c
@@ -1,20 +1,48 @@
 #include<stdio.h>
-#include<stdlib.h	>
+#include<stdlib.h>
+#include<string.h>
+
 void main()
 {
 FILE *fails;//deklarē ka būs fails
-float x = ((float)rand()/(float)(RAND_MAX)) * a;//sākums rasndof float generatoram
+float mass1[5][2];
+float mass2[5][3];
+int j, i;
 
-printf("Lūdzu ievadiet teikumu:");//nevajag
+for(int i=0;i<5;i++)
+  {
+  for(int j=0;j<2;j++)
+    {
+    printf("ievadiet %d kolonas %d rindas skaitli:", j+1 , i+1);
+    scanf("%f", &mass1[i][j]);
+    }
+  }
 
-fails = fopen("float_values.txt","w+");//atver failu rakstīšanai
-fprintf(fails,"%s", &);//raksta failā
-fclose(fails);//aizver failu
+fails = fopen("float_in_out_file.txt","w+");
+for( i=0;i<5;i++)
+  {
+  for( j=0;j<2;j++)
+    {
+    fprintf(fails,"%f ", mass1[i][j]);
+    }
+  fprintf(fails,"\n");
+  }
+fclose (fails);
 
-fails = fopen("float_values.txt", "r+");//atver lasīšanai
-fscanf(fails, "%[^\n]", );//skenē failu
-printf("nolasīts no faila: %s\n", );//izkopē to ko nolasīja no faila
-fclose(fails);//aizver failu
+fails = fopen("float_in_out_file.txt","r+");
+for(i=0;i<5;i++)
+  {
+  for(j=0;j<2;j++)
+    {
+    fscanf(fails,"%f", &mass2[i][j]);
+    }
+  }
+fclose (fails);
 
-
+printf("skaitļi no faila sareizināti:\n");
+for(i=0;i<5;i++)
+  {
+  printf("%f * %f = %f  ",mass2[i][0] ,mass2[i][1] ,mass2[i][0] * mass2[i][1]);
+  printf("\n");
+  }
 }
